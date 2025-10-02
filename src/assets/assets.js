48e64e2d9c5e4cdf01b9f6fa45526ed58cd92cbb
kalavathikarnati <kalavathikarnati2@gmail.com>
2025-10-03 00:57:57 +0530

    first commit

---
+++ src/assets/assets.js
@@ -0,0 +1,20 @@
+import add_icon from './plus-circle.svg';
+import logo from './Brand.png';
+import list_icon from './list-items.svg';
+import order_icon from './order-items.svg'
+import profile from './profile.svg'
+import upload_icon from './upload_area.png'
+import parcel_icon from './parcel_icon.png'
+
+export const url = "https://food-4h0a.onrender.com"
+export const assets ={
+
+    add_icon,
+    logo,
+    list_icon,
+    order_icon,
+    profile,
+    upload_icon,
+    parcel_icon
+
+}
